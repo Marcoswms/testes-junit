991db60c07822512b796cb3d6d2598163f1dfb0f
Marcos Willian <marcoswms@gmail.com>
2026-09-04 14:44:56 -0300

    test: adicionar testes padronizados para a classe Usuario

--- src/test/java/org/example/entities/CalculadoraTest.java
+++ src/test/java/org/example/entities/CalculadoraTest.java
@@ -1,12 +1,22 @@
 package org.example.entities;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
 
-    Calculadora calculadora = new Calculadora();
+    //static - Dados 'static' existirá apenas uma cópia para todas as instâncias, ou seja, apontam para o mesmo lugar de memória.
+    static Calculadora calculadora;
+
+    //Este bloco será executado uma única vez, porém o primeiro a ser executado.
+    @BeforeAll
+    static void criaCalculadora() {
+        calculadora = new Calculadora();
+    }
 
     @Test
     void soma() {
@@ -20,9 +30,10 @@ public class CalculadoraTest {
         assertEquals(2, divisao);
     }
 
-    @Test
-    void verificaSePar() {
-         boolean verifica = calculadora.verificaSePar(3);
-         assertTrue(verifica);
+    //Padroniza testes ao invés de repetir códigos com a mesma funcionalidade
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8})
+    void verificaSePar(int numero) {
+         assertTrue(calculadora.verificaSePar(numero));
     }
 }
